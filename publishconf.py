26e02c60763a99b4e0b3da2a3903bae853a6cb3d
Cynthia Li <home@ip-192-168-0-229.ec2.internal>
2021-10-03 20:01:36 -0600

    modified publishconf.pyg

--- publishconf.py
+++ publishconf.py
@@ -12,12 +12,12 @@ from pelicanconf import *
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = ''
 RELATIVE_URLS = False
-OUTPUT_PATH = "docs/"
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
-DELETE_OUTPUT_DIRECTORY = True
+OUTPUT_PATH = "docs/"
+# DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
